1eaa6aadeafc434b9d1c8024d40fe5451d3e7553
sinone-0801 <something.sinone@gmail.com>
2024-10-05 10:23:36 +0900

    Initial commit

--- script.js
+++ script.js
@@ -21,36 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
             project.style.transform = 'scale(1)';
         });
     });
-
-    // Formspreeを使用したフォーム送信
-    const form = document.getElementById('contact-form');
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const status = document.createElement('div');
-        status.classList.add('form-status');
-        form.appendChild(status);
-
-        fetch(form.action, {
-            method: form.method,
-            body: new FormData(form),
-            headers: {
-                'Accept': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                status.innerHTML = "ありがとうございます！メッセージが送信されました。";
-                form.reset();
-            } else {
-                response.json().then(data => {
-                    if (Object.hasOwn(data, 'errors')) {
-                        status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
-                    } else {
-                        status.innerHTML = "申し訳ありませんが、エラーが発生しました。後でもう一度お試しください。";
-                    }
-                });
-            }
-        }).catch(error => {
-            status.innerHTML = "申し訳ありませんが、エラーが発生しました。後でもう一度お試しください。";
-        });
-    });
 });
