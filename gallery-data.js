5c68857f9791e01e5bbdf6707e2797df1da922e4
chrytory <1md10119w@gmail.com>
2024-11-29 10:29:34 +0900

    add images

--- gallery-data.js
+++ gallery-data.js
@@ -1,56 +1,554 @@
 const GALLERY_ITEMS = [
   {
     "id": 1,
-    "src": "images/dc7ff658-109a-4876-9291-0de73bc3c4c0 (1).png",
-    "title": "Dc7Ff658 109A 4876 9291 0De73Bc3C4C0 (1)",
+    "src": "images/00003-1255464899.png",
+    "title": "00003 1255464899",
     "description": "AI生成による作品 1"
   },
   {
     "id": 2,
-    "src": "images/img1.png",
-    "title": "Img1",
+    "src": "images/00003-198278794.png",
+    "title": "00003 198278794",
     "description": "AI生成による作品 2"
   },
   {
     "id": 3,
-    "src": "images/img2.png",
-    "title": "Img2",
+    "src": "images/00005-2300826860.png",
+    "title": "00005 2300826860",
     "description": "AI生成による作品 3"
   },
   {
     "id": 4,
-    "src": "images/img3.png",
-    "title": "Img3",
+    "src": "images/00006-772077633.png",
+    "title": "00006 772077633",
     "description": "AI生成による作品 4"
   },
   {
     "id": 5,
-    "src": "images/img4.png",
-    "title": "Img4",
+    "src": "images/00007-4251638547.png",
+    "title": "00007 4251638547",
     "description": "AI生成による作品 5"
   },
   {
     "id": 6,
-    "src": "images/img5.jpg",
-    "title": "Img5",
+    "src": "images/00013-4078754178.png",
+    "title": "00013 4078754178",
     "description": "AI生成による作品 6"
   },
   {
     "id": 7,
-    "src": "images/img6.jpg",
-    "title": "Img6",
+    "src": "images/00032-1179048870.png",
+    "title": "00032 1179048870",
     "description": "AI生成による作品 7"
   },
   {
     "id": 8,
-    "src": "images/img7.jpg",
-    "title": "Img7",
+    "src": "images/00033-4023280289.png",
+    "title": "00033 4023280289",
     "description": "AI生成による作品 8"
   },
   {
     "id": 9,
+    "src": "images/00052-3925094053.png",
+    "title": "00052 3925094053",
+    "description": "AI生成による作品 9"
+  },
+  {
+    "id": 10,
+    "src": "images/00074-3610940740.png",
+    "title": "00074 3610940740",
+    "description": "AI生成による作品 10"
+  },
+  {
+    "id": 11,
+    "src": "images/00133-869417512.png",
+    "title": "00133 869417512",
+    "description": "AI生成による作品 11"
+  },
+  {
+    "id": 12,
+    "src": "images/240504_1.png",
+    "title": "240504 1",
+    "description": "AI生成による作品 12"
+  },
+  {
+    "id": 13,
+    "src": "images/ComfyUI_00035_.png",
+    "title": "Comfyui 00035 ",
+    "description": "AI生成による作品 13"
+  },
+  {
+    "id": 14,
+    "src": "images/ComfyUI_00063_.png",
+    "title": "Comfyui 00063 ",
+    "description": "AI生成による作品 14"
+  },
+  {
+    "id": 15,
+    "src": "images/ComfyUI_00085_.png",
+    "title": "Comfyui 00085 ",
+    "description": "AI生成による作品 15"
+  },
+  {
+    "id": 16,
+    "src": "images/ComfyUI_00087_.png",
+    "title": "Comfyui 00087 ",
+    "description": "AI生成による作品 16"
+  },
+  {
+    "id": 17,
+    "src": "images/ComfyUI_00092_.png",
+    "title": "Comfyui 00092 ",
+    "description": "AI生成による作品 17"
+  },
+  {
+    "id": 18,
+    "src": "images/ComfyUI_00122_.png",
+    "title": "Comfyui 00122 ",
+    "description": "AI生成による作品 18"
+  },
+  {
+    "id": 19,
+    "src": "images/ComfyUI_00157_.png",
+    "title": "Comfyui 00157 ",
+    "description": "AI生成による作品 19"
+  },
+  {
+    "id": 20,
+    "src": "images/ComfyUI_00161_.png",
+    "title": "Comfyui 00161 ",
+    "description": "AI生成による作品 20"
+  },
+  {
+    "id": 21,
+    "src": "images/ComfyUI_00167_.png",
+    "title": "Comfyui 00167 ",
+    "description": "AI生成による作品 21"
+  },
+  {
+    "id": 22,
+    "src": "images/ComfyUI_00370_.png",
+    "title": "Comfyui 00370 ",
+    "description": "AI生成による作品 22"
+  },
+  {
+    "id": 23,
+    "src": "images/ComfyUI_00373_.png",
+    "title": "Comfyui 00373 ",
+    "description": "AI生成による作品 23"
+  },
+  {
+    "id": 24,
+    "src": "images/ComfyUI_00380_.png",
+    "title": "Comfyui 00380 ",
+    "description": "AI生成による作品 24"
+  },
+  {
+    "id": 25,
+    "src": "images/ComfyUI_00381_.png",
+    "title": "Comfyui 00381 ",
+    "description": "AI生成による作品 25"
+  },
+  {
+    "id": 26,
+    "src": "images/ComfyUI_00382_.png",
+    "title": "Comfyui 00382 ",
+    "description": "AI生成による作品 26"
+  },
+  {
+    "id": 27,
+    "src": "images/ComfyUI_00386_.png",
+    "title": "Comfyui 00386 ",
+    "description": "AI生成による作品 27"
+  },
+  {
+    "id": 28,
+    "src": "images/ComfyUI_00389_.png",
+    "title": "Comfyui 00389 ",
+    "description": "AI生成による作品 28"
+  },
+  {
+    "id": 29,
+    "src": "images/ComfyUI_01019_.png",
+    "title": "Comfyui 01019 ",
+    "description": "AI生成による作品 29"
+  },
+  {
+    "id": 30,
+    "src": "images/ComfyUI_01045_.png",
+    "title": "Comfyui 01045 ",
+    "description": "AI生成による作品 30"
+  },
+  {
+    "id": 31,
+    "src": "images/ComfyUI_01063_.png",
+    "title": "Comfyui 01063 ",
+    "description": "AI生成による作品 31"
+  },
+  {
+    "id": 32,
+    "src": "images/ComfyUI_01065_.png",
+    "title": "Comfyui 01065 ",
+    "description": "AI生成による作品 32"
+  },
+  {
+    "id": 33,
+    "src": "images/ComfyUI_01067_.png",
+    "title": "Comfyui 01067 ",
+    "description": "AI生成による作品 33"
+  },
+  {
+    "id": 34,
+    "src": "images/ComfyUI_01105_.png",
+    "title": "Comfyui 01105 ",
+    "description": "AI生成による作品 34"
+  },
+  {
+    "id": 35,
+    "src": "images/ComfyUI_01109_.png",
+    "title": "Comfyui 01109 ",
+    "description": "AI生成による作品 35"
+  },
+  {
+    "id": 36,
+    "src": "images/ComfyUI_01176_.png",
+    "title": "Comfyui 01176 ",
+    "description": "AI生成による作品 36"
+  },
+  {
+    "id": 37,
+    "src": "images/ComfyUI_01212_.png",
+    "title": "Comfyui 01212 ",
+    "description": "AI生成による作品 37"
+  },
+  {
+    "id": 38,
+    "src": "images/ComfyUI_01248_.png",
+    "title": "Comfyui 01248 ",
+    "description": "AI生成による作品 38"
+  },
+  {
+    "id": 39,
+    "src": "images/ComfyUI_01410_.png",
+    "title": "Comfyui 01410 ",
+    "description": "AI生成による作品 39"
+  },
+  {
+    "id": 40,
+    "src": "images/ComfyUI_01453_.png",
+    "title": "Comfyui 01453 ",
+    "description": "AI生成による作品 40"
+  },
+  {
+    "id": 41,
+    "src": "images/ComfyUI_01488_.png",
+    "title": "Comfyui 01488 ",
+    "description": "AI生成による作品 41"
+  },
+  {
+    "id": 42,
+    "src": "images/ComfyUI_01492_.png",
+    "title": "Comfyui 01492 ",
+    "description": "AI生成による作品 42"
+  },
+  {
+    "id": 43,
+    "src": "images/ComfyUI_01614_.png",
+    "title": "Comfyui 01614 ",
+    "description": "AI生成による作品 43"
+  },
+  {
+    "id": 44,
+    "src": "images/ComfyUI_01621_.png",
+    "title": "Comfyui 01621 ",
+    "description": "AI生成による作品 44"
+  },
+  {
+    "id": 45,
+    "src": "images/ComfyUI_01885_.png",
+    "title": "Comfyui 01885 ",
+    "description": "AI生成による作品 45"
+  },
+  {
+    "id": 46,
+    "src": "images/ComfyUI_02011_.png",
+    "title": "Comfyui 02011 ",
+    "description": "AI生成による作品 46"
+  },
+  {
+    "id": 47,
+    "src": "images/ComfyUI_02037_.png",
+    "title": "Comfyui 02037 ",
+    "description": "AI生成による作品 47"
+  },
+  {
+    "id": 48,
+    "src": "images/ComfyUI_02043_.png",
+    "title": "Comfyui 02043 ",
+    "description": "AI生成による作品 48"
+  },
+  {
+    "id": 49,
+    "src": "images/ComfyUI_02050_.png",
+    "title": "Comfyui 02050 ",
+    "description": "AI生成による作品 49"
+  },
+  {
+    "id": 50,
+    "src": "images/ComfyUI_02064_.png",
+    "title": "Comfyui 02064 ",
+    "description": "AI生成による作品 50"
+  },
+  {
+    "id": 51,
+    "src": "images/ComfyUI_02069_.png",
+    "title": "Comfyui 02069 ",
+    "description": "AI生成による作品 51"
+  },
+  {
+    "id": 52,
+    "src": "images/ComfyUI_02106_.png",
+    "title": "Comfyui 02106 ",
+    "description": "AI生成による作品 52"
+  },
+  {
+    "id": 53,
+    "src": "images/ComfyUI_02114_.png",
+    "title": "Comfyui 02114 ",
+    "description": "AI生成による作品 53"
+  },
+  {
+    "id": 54,
+    "src": "images/ComfyUI_temp_hprrb_00018_.png",
+    "title": "Comfyui Temp Hprrb 00018 ",
+    "description": "AI生成による作品 54"
+  },
+  {
+    "id": 55,
+    "src": "images/ComfyUI_temp_iyeuh_00023_.png",
+    "title": "Comfyui Temp Iyeuh 00023 ",
+    "description": "AI生成による作品 55"
+  },
+  {
+    "id": 56,
+    "src": "images/ComfyUI_temp_llynl_00002_t.png",
+    "title": "Comfyui Temp Llynl 00002 T",
+    "description": "AI生成による作品 56"
+  },
+  {
+    "id": 57,
+    "src": "images/ComfyUI_temp_sqplj_00008_.png",
+    "title": "Comfyui Temp Sqplj 00008 ",
+    "description": "AI生成による作品 57"
+  },
+  {
+    "id": 58,
+    "src": "images/ComfyUI_temp_sqplj_00018_.png",
+    "title": "Comfyui Temp Sqplj 00018 ",
+    "description": "AI生成による作品 58"
+  },
+  {
+    "id": 59,
+    "src": "images/ComfyUI_temp_sqplj_00023_.png",
+    "title": "Comfyui Temp Sqplj 00023 ",
+    "description": "AI生成による作品 59"
+  },
+  {
+    "id": 60,
+    "src": "images/ComfyUI_temp_sqplj_00078_.png",
+    "title": "Comfyui Temp Sqplj 00078 ",
+    "description": "AI生成による作品 60"
+  },
+  {
+    "id": 61,
+    "src": "images/ComfyUI_temp_sqplj_00090_.png",
+    "title": "Comfyui Temp Sqplj 00090 ",
+    "description": "AI生成による作品 61"
+  },
+  {
+    "id": 62,
+    "src": "images/ComfyUI_temp_zztmu_00006_.png",
+    "title": "Comfyui Temp Zztmu 00006 ",
+    "description": "AI生成による作品 62"
+  },
+  {
+    "id": 63,
+    "src": "images/ComfyUI_temp_zztmu_00015_.png",
+    "title": "Comfyui Temp Zztmu 00015 ",
+    "description": "AI生成による作品 63"
+  },
+  {
+    "id": 64,
+    "src": "images/ComfyUI_temp_zztmu_00043_.png",
+    "title": "Comfyui Temp Zztmu 00043 ",
+    "description": "AI生成による作品 64"
+  },
+  {
+    "id": 65,
+    "src": "images/ComfyUI_temp_zztmu_00053_.png",
+    "title": "Comfyui Temp Zztmu 00053 ",
+    "description": "AI生成による作品 65"
+  },
+  {
+    "id": 66,
+    "src": "images/ComfyUI_temp_zztmu_00058_.png",
+    "title": "Comfyui Temp Zztmu 00058 ",
+    "description": "AI生成による作品 66"
+  },
+  {
+    "id": 67,
+    "src": "images/ComfyUI_temp_zztmu_00063_.png",
+    "title": "Comfyui Temp Zztmu 00063 ",
+    "description": "AI生成による作品 67"
+  },
+  {
+    "id": 68,
+    "src": "images/dc7ff658-109a-4876-9291-0de73bc3c4c0 (1).png",
+    "title": "Dc7Ff658 109A 4876 9291 0De73Bc3C4C0 (1)",
+    "description": "AI生成による作品 68"
+  },
+  {
+    "id": 69,
+    "src": "images/icon.png",
+    "title": "Icon",
+    "description": "AI生成による作品 69"
+  },
+  {
+    "id": 70,
+    "src": "images/img1.png",
+    "title": "Img1",
+    "description": "AI生成による作品 70"
+  },
+  {
+    "id": 71,
+    "src": "images/img2.png",
+    "title": "Img2",
+    "description": "AI生成による作品 71"
+  },
+  {
+    "id": 72,
+    "src": "images/img3.png",
+    "title": "Img3",
+    "description": "AI生成による作品 72"
+  },
+  {
+    "id": 73,
+    "src": "images/img4.png",
+    "title": "Img4",
+    "description": "AI生成による作品 73"
+  },
+  {
+    "id": 74,
+    "src": "images/img5.jpg",
+    "title": "Img5",
+    "description": "AI生成による作品 74"
+  },
+  {
+    "id": 75,
+    "src": "images/img6.jpg",
+    "title": "Img6",
+    "description": "AI生成による作品 75"
+  },
+  {
+    "id": 76,
+    "src": "images/img7.jpg",
+    "title": "Img7",
+    "description": "AI生成による作品 76"
+  },
+  {
+    "id": 77,
     "src": "images/img8.jpg",
     "title": "Img8",
-    "description": "AI生成による作品 9"
+    "description": "AI生成による作品 77"
+  },
+  {
+    "id": 78,
+    "src": "images/sinone_icon.jpg",
+    "title": "Sinone Icon",
+    "description": "AI生成による作品 78"
+  },
+  {
+    "id": 79,
+    "src": "images/sinone_jr_header.png",
+    "title": "Sinone Jr Header",
+    "description": "AI生成による作品 79"
+  },
+  {
+    "id": 80,
+    "src": "images/tmp64i_vufs.png",
+    "title": "Tmp64I Vufs",
+    "description": "AI生成による作品 80"
+  },
+  {
+    "id": 81,
+    "src": "images/tmp80fasjma.png",
+    "title": "Tmp80Fasjma",
+    "description": "AI生成による作品 81"
+  },
+  {
+    "id": 82,
+    "src": "images/tmp8v7jbb23.png",
+    "title": "Tmp8V7Jbb23",
+    "description": "AI生成による作品 82"
+  },
+  {
+    "id": 83,
+    "src": "images/tmpc38axht7.png",
+    "title": "Tmpc38Axht7",
+    "description": "AI生成による作品 83"
+  },
+  {
+    "id": 84,
+    "src": "images/tmpcs76ohn9.png",
+    "title": "Tmpcs76Ohn9",
+    "description": "AI生成による作品 84"
+  },
+  {
+    "id": 85,
+    "src": "images/tmpeag8r07l.png",
+    "title": "Tmpeag8R07L",
+    "description": "AI生成による作品 85"
+  },
+  {
+    "id": 86,
+    "src": "images/tmpf0wklac8.png",
+    "title": "Tmpf0Wklac8",
+    "description": "AI生成による作品 86"
+  },
+  {
+    "id": 87,
+    "src": "images/tmpgf4oc8in.png",
+    "title": "Tmpgf4Oc8In",
+    "description": "AI生成による作品 87"
+  },
+  {
+    "id": 88,
+    "src": "images/tmpk_ey30l0.png",
+    "title": "Tmpk Ey30L0",
+    "description": "AI生成による作品 88"
+  },
+  {
+    "id": 89,
+    "src": "images/tmppg6ajz44.png",
+    "title": "Tmppg6Ajz44",
+    "description": "AI生成による作品 89"
+  },
+  {
+    "id": 90,
+    "src": "images/tmppn19ymz3.png",
+    "title": "Tmppn19Ymz3",
+    "description": "AI生成による作品 90"
+  },
+  {
+    "id": 91,
+    "src": "images/tmpz1v9441o.png",
+    "title": "Tmpz1V9441O",
+    "description": "AI生成による作品 91"
+  },
+  {
+    "id": 92,
+    "src": "images/ダウンロード.png",
+    "title": "ダウンロード",
+    "description": "AI生成による作品 92"
   }
 ];
